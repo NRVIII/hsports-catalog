8ae2273e6edbef24f84f0b6d8bf6a94984ee6280
nrviii <nrviii@outlook.com>
2020-10-21 11:32:37 -0300

    Created a JSF to return a better response page

--- hsports-catalog-servlet/src/main/java/com/linkedin/servlet/CatalogServlet.java
+++ hsports-catalog-servlet/src/main/java/com/linkedin/servlet/CatalogServlet.java
@@ -1,6 +1,7 @@
 package com.linkedin.servlet;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -10,40 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "CatalogServlet", asyncSupported = true)
+@WebServlet(urlPatterns = "CatalogServlet")
 public class CatalogServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        AsyncContext asyncContext = request.startAsync();
-
-        asyncContext.start(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-
-                    System.out.println("Print the response");
-                    System.out.println("Response returned by: " + Thread.currentThread().getName());
-
-                    returnResponse(request, response);
-
-                    asyncContext.complete();
-                } catch (InterruptedException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        System.out.println("Initial Request: " + Thread.currentThread().getName());
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append(request.getParameter("name"));
-        response.setHeader("someHeader", "someHeaderValue");
-        response.addCookie(new Cookie("someCookie", "someCookieValue"));
-    }
-
-    private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
         String manufacturer = request.getParameter("manufacturer");
         String sku = request.getParameter("sku");
@@ -51,30 +22,16 @@ public class CatalogServlet extends HttpServlet {
         response.setHeader("someHeader", "someHeaderValue");
         response.addCookie(new Cookie("someCookie", "someCookieValue"));
 
-        Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+        Catalog.addItem(new CatalogItem(name, manufacturer,sku));
 
-        PrintWriter out = response.getWriter();
+        request.setAttribute("items", Catalog.getItems());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+        dispatcher.forward(request, response);
+    }
 
-        out.println("<html>");
-        out.println("<head></head>");
-        out.println("<body>");
-        out.println("<table>");
-
-        for (CatalogItem item : Catalog.getItems()) {
-            out.println("<tr>");
-            out.println("<td>");
-            out.print(item.getName());
-            out.println("</td>");
-            out.println("<td>");
-            out.print(item.getManufacturer());
-            out.println("</td>");
-            out.println("<td>");
-            out.print(item.getSku());
-            out.println("</td>");
-            out.println("</tr>");
-        }
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append(request.getParameter("name"));
+        response.setHeader("someHeader", "someHeaderValue");
+        response.addCookie(new Cookie("someCookie", "someCookieValue"));
     }
 }
--- hsports-catalog-servlet/src/main/java/com/linkedin/servlet/ParameterLoggingFilter.java
+++ hsports-catalog-servlet/src/main/java/com/linkedin/servlet/ParameterLoggingFilter.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/*", asyncSupported = true)
+@WebFilter(urlPatterns = "/*")
 public class ParameterLoggingFilter implements Filter {
     public void destroy() {
     }
